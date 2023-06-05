08604b251ce58af423ba6f7cc87f28403bc7b295
ZhouNan <zhounan@1utar.my>
2023-06-05 11:49:26 +0800

    3

--- main2.py
+++ main2.py
@@ -1,87 +1,83 @@
-import streamlit as st
 import pandas as pd
-from api import OpenAI_API, open_ai_response
-from question_bank import question_bank
+import numpy as np
 
-def page_config():
-    st.set_page_config(page_title="ChatGPT Data Assistant", page_icon="📊", layout="centered")
-    hide_menu_style = "<style> footer {visibility: hidden;} </style>"
-    st.markdown(hide_menu_style, unsafe_allow_html=True)
-
-
-def sidebar():
-    st.sidebar.title('About')
-    st.sidebar.info('''
-        This app uses the [OpenAI API](https://beta.openai.com/) to generate responses to questions about data files.
-        ''')
-    st.sidebar.title('Guide')
-    st.sidebar.info('''
-        1. 可以上传csv或者excel文件
-        2. Select a prompt from the dropdown menu.
-        3. Click the "Generate response" button.
-        ''')
-    st.text(" ")
-    st.sidebar.markdown(
-    """
-    <a href="https://twitter.com/cameronjoejones" target="_blank" style="text-decoration: none;">
-        <div style="display: flex; align-items: center;">
-            <img src="https://abs.twimg.com/icons/apple-touch-icon-192x192.png" width="30" height="30">
-            <span style="font-size: 16px; margin-left: 5px;">Follow me on Twitter</span>
-        </div>
-    </a>
-    """, unsafe_allow_html=True
-    )
-    
+# 导入pandasai
+from pandasai import PandasAI
+from pandasai.llm.openai import OpenAI
+# 导入streamlit
+import streamlit as st
 
 
-def run_app():
-    api_key = st.secrets["api_key"]['open_ai']
-    if api_key is None:
-        st.error('API key not found. Please set the api_key in the .streamlit/secrets.toml file.')
-    else:
-        with OpenAI_API(api_key):
-            st.title('🤵🏻‍♂️ ChatGPT Data Assistant')
 
-            example_data = st.checkbox('Use example data', value=False)
 
-            if example_data == True:
-                upload_file = r'input/example_data.csv'
-                st.write(pd.read_csv(upload_file))
+class MyApp():
+    def __init__(self):
+        self.prompt = None
+        self.currentdf = None
+        self.sheet_list = None
+        self.sheet = None
+        
+        self.data = None
+        self.title = "PandasAI"
+        self.file = None
+        self.secret = st.secrets["api"]["api"]
+        self.API = "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"
+        self.llm = OpenAI(api_token=self.secret)
+        self.pandas_ai = PandasAI(self.llm)
 
-            elif example_data == False:
-                upload_file = st.file_uploader("Upload your data file", type='xlsx,xls,csv')
-
-            prompt_selected = st.selectbox('Select a prompt', list(question_bank.keys()))
-            prompt = question_bank[prompt_selected]['question']
-            st.write(prompt)
-            submit = st.button('Generate response')
-
+    def run(self):
+        self.uploadfile()
+        if self.file is not None:
             
-            if submit:
-                with st.spinner('Generating response...'):            
-                    if upload_file is not None:
-                        if example_data == False:
-                            if upload_file.name.endswith('.csv'):
-                                df = pd.read_csv(upload_file)
-                            elif upload_file.name.endswith('.xlsx') or upload_file.name.endswith('.xls'):
-                                df = pd.read_excel(upload_file)
-                        elif example_data == True:
-                            df = pd.read_csv(upload_file)
-                        
-                        prompt = question_bank[prompt_selected]['question']
-                        answer = open_ai_response(prompt, df)
-                        st.markdown('### Ouput:')
-                        st.write(answer)
-                        # st.text_area(label='', value=answer, height=200)
+            
+            self.df()
+            self.prom()
+            self.output()
 
-                        print(answer)
-                    else:
-                        st.error('Please upload a data file')
-                        st.stop()
-                    return df
+    def uploadfile(self):
+        self.file = st.file_uploader('上传文件', type=['xls', 'xlsx'])
+
+        if self.file is not None:
+            self.data = pd.ExcelFile(self.file)
+            self.data_dict = {}
+            for sheet in self.data.sheet_names:
+                self.data_dict[sheet] = self.data.parse(sheet)
+
+
+    def sheetselect(self):
+        self.sheet_list = list(self.data_dict.keys())
+        self.sheet = st.selectbox('选择sheet', self.sheet_list)
+        return self.sheet
+
+    def df(self):
+        self.sheetselect()
+        self.currentdf = self.data_dict[self.sheet]
+        st.write(self.currentdf)
+
+
     
+        
+    def prom(self):
+        problem = st.text_input('输入问题')
+        self.prompt = (
+            "背景：使用我提供的dataframe。"
+            "任务：根据我提供的指令对dataframe进行操作。"
+            "信息：一般情况下跳过NaN值，除非我特别要求。"
+            "情境：我的问题通常与筛选数据或描述性统计有关。"
+            "指令：{problem}。"
+            "执行：尽可能以markdown表格输出结果，并且尽可能输出完整的结果。你的描述性语句要使用中文。".format(
+                problem=problem))
+    def output(self):
+        if st.button('执行'):
+
+            output = self.pandas_ai.run(self.currentdf, prompt=self.prompt)
+
+            st.write(output)
+            st.markdown(output)
+
+
+
 
 if __name__ == '__main__':
-    page_config()
-    run_app()
-    sidebar()
+    app = MyApp()
+    app.run()
